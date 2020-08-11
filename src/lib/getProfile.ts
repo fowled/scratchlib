6a231d3d44a30c9c6cb068a3fc7c09b6b7f93b8e
Mazz <ma15fo43@gmail.com>
2020-08-11 16:36:10 +0200

    Update getProfile.ts

--- src/lib/getProfile.ts
+++ src/lib/getProfile.ts
@@ -1,5 +1,17 @@
-async function getProfile(username: string) {
+import * as fetch from "node-fetch";
 
+async function getProfile(username: string) {
+    if (!username) {
+        throw "Parameter of type string is missing";
+    } else if (typeof(username) !== string) {
+        throw "Parameter specified must be of type string.";
+    }
+
+    let url = `https://api.scratch.mit.edu/username/${username}`;
+    let req = await fetch(url);
+    let parsedJSON = await req.json();
+   
+    return parsedJSON;
 }
 
 export { getProfile };
