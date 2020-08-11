033d4a1a7bdbc35212462169ec3f8d02b31f3647
ignmazz <ma15fo43@gmail.com>
2020-08-11 21:38:41 +0200

    commits go brr

--- src/lib/getProfile.ts
+++ src/lib/getProfile.ts
@@ -1,5 +1,17 @@
-async function getProfile(username: string) {
+import fetch from "node-fetch";
 
+async function getProfile(username: string) {
+    if (!username) {
+        return console.error("Parameter of type string is missing");
+    } else if (typeof username !== "string") {
+        return console.error("Parameter specified must be of type string.");
+    }
+
+    let url = `https://api.scratch.mit.edu/users/${username}`;
+    let req = await fetch(url);
+    let parsedJSON = await req.json();
+   
+    return parsedJSON;
 }
 
 export { getProfile };
--- src/lib/getStatus.ts
+++ src/lib/getStatus.ts
@@ -1,36 +1,11 @@
-import { XMLHttpRequest } from "xmlhttprequest-ts";
+import fetch from "node-fetch";
 
 async function getStatus() {
-    var xhr = new XMLHttpRequest();
+    let url = "https://api.scratch.mit.edu/health";
+    let response = await fetch(url);
+    let parsedJSON = await response.json();
 
-    xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            let parsed = JSON.parse(this.responseText);
-            let data: {} = {
-                uptime: getDateTimestamp(parsed.uptime),
-                version: parsed.version,
-                load: [parsed.load],
-                sql: { ssl: parsed.sql.ssl, started: parsed.sql.ssl, min: parsed.sql.min, max: parsed.sql.max }
-            };
-
-            return data;
-        } else if (this.readyState == 4 && this.status != 200) {
-            return `Request to https://api.scratch.mit.edu/health failed. Status: ${this.status}`;
-        }
-    };
-
-    xhr.open("GET", "https://api.scratch.mit.edu/health", true);
-    xhr.send();
-}
-
-function getDateTimestamp(timestamp: any) {
-    let date = new Date(timestamp * 1000);
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    let seconds = date.getSeconds();
-    let formattedTime = `${hours}:${minutes}:${seconds}`;
-
-    return formattedTime;
+    return parsedJSON;
 }
 
 export { getStatus };
