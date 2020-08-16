c77afc353ae767c6fd722e5899b1e11e0d1f4168
ma15fo43 <ma15fo43@gmail.com>
2020-08-16 12:42:31 +0200

    added type definitions

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -1,6 +1,19 @@
-declare module 'scratchlib' {
-    export function getProfile(user: string): Promise<JSON>;
-    export let parsedJSON: JSON;
+// Type definitions for scratchlib module
+// Project: ScratchLib-core
+// Definitions by: mazzlabs - https://mazz.fun
 
+declare module 'scratchlib' {
+    export function getFeatured(): Promise<JSON>;
+    export function getNews(): Promise<JSON>;
+    export function getProfile(user: string): Promise<JSON>;
+    export function getProject(id: string): Promise<JSON>;
+    export function getProjectsCount(): Promise<JSON>;
+    export function getRoot(): Promise<JSON>;
     export function getStatus(): Promise<JSON>;
+    export function getUserActivity(user: string): Promise<JSON>;
+    export function getUserFavorites(user: string): Promise<JSON>;
+    export function getUserFollowers(user: string): Promise<JSON>;
+    export function getUserFollowing(user: string): Promise<JSON>;
+    export function getUserMessages(user: string): Promise<JSON>;
+    export function getUserProjects(user: string): Promise<JSON>;
 }
