019ec66acf4ab35bc0dfdb3c923361c15a5ee50b
ma15fo43 <ma15fo43@gmail.com>
2020-10-21 14:04:34 +0200

    🧪 Added new tests

--- tests/main.js
+++ tests/main.js
@@ -1,6 +1,7 @@
 const ScratchLib = require("../index.js");
+require("dotenv").config();
 
-ScratchLib.getUserMessages().then(info => {
+ScratchLib.getFeatured("http://localhost:8080").then(info => {
 	console.log(info);
 }).catch(err => {
 	console.error(err);
