0685d9ad9f5b6c8b84d2f1c6c80779e6c5ea4acb
ma15fo43 <ma15fo43@gmail.com>
2020-10-21 14:02:56 +0200

    📘 Added new types (Allow setting a URL prefix #3)

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -3,16 +3,16 @@
 // Definitions by: mazzlabs - https://mazz.fun
 
 declare module 'scratchlib' {
-    export function getFeatured(): Promise<JSON>;
-    export function getNews(): Promise<JSON>;
-    export function getProfile(user: string): Promise<JSON>;
-    export function getProject(id: string): Promise<JSON>;
-    export function getProjectsCount(): Promise<JSON>;
-    export function getRoot(): Promise<JSON>;
-    export function getStatus(): Promise<JSON>;
-    export function getUserFavorites(user: string): Promise<JSON>;
-    export function getUserFollowers(user: string): Promise<JSON>;
-    export function getUserFollowing(user: string): Promise<JSON>;
-    export function getUserMessages(user: string): Promise<JSON>;
-    export function getUserProjects(user: string): Promise<JSON>;
+    export function getFeatured(proxy?: string): Promise<JSON>;
+    export function getNews(proxy?: string): Promise<JSON>;
+    export function getProfile(user: string, proxy?: string): Promise<JSON>;
+    export function getProject(id: string, proxy?: string): Promise<JSON>;
+    export function getProjectsCount(proxy?: string): Promise<JSON>;
+    export function getRoot(proxy?: string): Promise<JSON>;
+    export function getStatus(proxy?: string): Promise<JSON>;
+    export function getUserFavorites(user: string, proxy?: string): Promise<JSON>;
+    export function getUserFollowers(user: string, proxy?: string): Promise<JSON>;
+    export function getUserFollowing(user: string, proxy?: string): Promise<JSON>;
+    export function getUserMessages(user: string, proxy?: string): Promise<JSON>;
+    export function getUserProjects(user: string, proxy?: string): Promise<JSON>;
 }
